bf58ddb7f0d4f1cce974fff727d6fdce312a737b
Suhrob701 <suhrobnurmamatov909@gmail.com>
2025-04-07 21:35:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// document.getElementById('btn').addEventListener('click', async () => {
+//     const parm = await Notification.requestPermission();
+    
+//     if (parm == 'granted') {
+//       new Notification('Hi!', {
+//         body: 'Nma gap'
+//       });
+//     }
+// });
+  
